bc99e51e3fc60bfb7db113b0f8ffa12f78902fb7
Okan Çilingiroğlu <okan.cilingiroglu@gmail.com>
2021-12-12 11:46:28 +0300

    Refactor RabbitMQ Bus

--- MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
+++ MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
@@ -13,6 +13,12 @@ namespace MicroRabbit.Transfer.Data.Repository
             _dbContext = dbContext;
         }
 
+        public void Add(TransferLog transferLog)
+        {
+            _dbContext.TransferLogs.Add(transferLog);
+            _dbContext.SaveChanges();
+        }
+
         public IEnumerable<TransferLog> GetTransferLogs()
         {
             return _dbContext.TransferLogs;
